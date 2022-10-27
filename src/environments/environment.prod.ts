0a6d67f7f8b4ad2a55c7c45a298bff81e65d3e86
oite.diresa.cajamarca@gmail.com <oite.diresa.cajamarca@gmail.com>
2022-10-27 09:26:59 -0500

    27 de octubre 2022

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   url_api_django:'http://localhost:8000/api/',
-  url_api_node:'http://sir.diresacajamarca.gob.pe:8089/api/'
+  url_api_node:'http://sir.diresacajamarca.gob.pe:8089/ninio/'
 };
